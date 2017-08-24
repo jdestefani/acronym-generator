05f92cad5ae37ac0a7e8a1ad9c38a192fc4fb3c3
Jacopo De Stefani <jacopo.de.stefani@gmail.com>
2017-08-25 00:39:22 +0200

    Added actual code and check on python version > 3.

---
+++ AAG.py
@@ -0,0 +1,20 @@
+import itertools
+import sys
+
+if sys.version_info[0] < 3:
+            raise Exception("Python 3 or a more recent version is required.")
+
+longVersion = input("Insert the complete title of the article:")
+tokens = longVersion.split(sep=" ")
+uniqueLetters = [list(set(token)) for token in tokens]
+
+print("[INFO] - Loading dictionary")
+with open("wordsEn.txt") as word_file:
+        dictionary = set(word.strip().lower() for word in word_file)
+
+print("[INFO] - Generating acronyms")        
+for element in itertools.product(*uniqueLetters):
+    acronymn = "".join(element).lower()
+    if(acronymn in dictionary):
+        print("Found acronymn: " + acronymn)
+    
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# acronym-generator
+# acronymn-generator
+Simple dictionary-based acronymn generator written in Python (>=3).
+
+## How to run it?
+1. ```python3 AAG.py```
+2. Insert the (English) phrase you want to find a summarizing acronymn for
+
+## Example
+```
+Insert the complete title of the article:Distributed cooperation in wireless sensor networks
+[INFO] - Loading dictionary
+[INFO] - Generating acronyms
+Found acronymn: raiser
+Found acronymn: raises
+...
+```
